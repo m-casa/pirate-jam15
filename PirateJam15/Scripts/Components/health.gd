9a7b939fdd8db4ea2cc11e89598a3e50da3e91a2
Marc Casanova <marc.casanova@yahoo.com>
2024-07-25 04:37:27 -0500

    Added a red flash to enemies when they are hit

--- PirateJam15/Scripts/Components/health.gd
+++ PirateJam15/Scripts/Components/health.gd
@@ -3,6 +3,7 @@ class_name Health extends Node3D
 
 @export var _max_health = 10.0
 @export var _stun_duration = 0.3
+@export var _sprite = AnimatedSprite3D
 
 var _stunned: bool
 var _health: float
@@ -16,17 +17,22 @@ func _ready():
 	_timer = $Timer
 
 func damage_enemy(attack: Attack):
-	var enemy = get_parent()
-	
 	# Check if stunned, or else enemy might be hit multiple times 
 	#  within a few frames of each other
-	if _health > 0 and not _stunned:
+	if not _stunned:
+		var enemy = get_parent()
+		
 		_health -= attack.attack_damage
+		_sprite.modulate = Color.RED
 		enemy.setup_knockback(attack)
 		
 		_stunned = true
 		_timer.set_wait_time(_stun_duration)
 		_timer.start()
+		
+		# Start a separate timer for the enemy to regain its original color
+		await get_tree().create_timer(_timer.time_left).timeout
+		_sprite.modulate = Color.WHITE
 	
 	if _health <= 0:
 		get_parent().queue_free()
